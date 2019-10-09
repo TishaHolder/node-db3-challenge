a5c15bf504c7f44fed706eb0f538304401d0bbe8
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-10-09 15:40:21 -0400

    completed mvp

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -19,40 +19,55 @@ function find(){
 
 function findById(id){
     return db('schemes')
-    .where({ id })
+    //this is saying where id = id. could write instead {'schemes.id': id} which means where the id in the schemes 
+    //table is eqqual to the id entered
+    .where({'schemes.id': id}) 
     .first();
 }
 
 function findSteps(id){
-    return db('steps')
-    .where({ scheme_id: id })
-    .orderBy( 'steps.step_number' );
+    return db('schemes')
+    .join('steps', 'schemes.id', '=','steps.scheme_id',   )//join tables schemes and steps on both id fields
+    .where({ 'schemes.id': id }) //where the id in the schemes table is equal to the id entered
+    //the fields to display in the results
+    //display these field names in the results
+    .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+    .orderBy( 'steps.step_number' ); //order the steps in chronological order
 }
 
 function add(scheme){
     return db('schemes')
-    .insert(scheme)
-    .then( ([id]) => {
-        findById(id);
+    //'id' tells the database to retun the id after it inserts the record 
+    //important for postgres
+    .insert(scheme, 'id')
+    .then( ([id]) => { //insert returns id of the newly inserted row
+        return findById(id);
+    })
+    .catch(error => {
+        console.log("insert error", error);
     })
 }
 
 function update(changes, id){
     return db('schemes')
-    .where('id', id)
+    .where('id', id)//where the id in the schemes table is equal to the id that was entered
     .update(changes)
     .then( count => {
-        count > 0 ? this.findById(id) : null;      
+        return count > 0 ? findById(id) : null;      
     })
 
 }
 
 function remove(id){
+
+    const record = {};
+
     return db('schemes')
-    .where('id', id)
-    .delete()    
+    .where('id', id)//where the id in the schemes table is equal to the id that was entered    
+    .delete()  
     .then( count => {
-        count > 0 ? this.findById(id) : null;   
+        //return count > 0 ? findById(id) : null;  
+        return count > 0 ? count : null;    
     })
 
 }
